f14fdcb6389c93d78710d9841c832544d79f6207
Leon Cheung <leonjcheung@gmail.com>
2017-02-11 12:45:29 -0800

    fix indentation, general cleanup, filename

--- src/pushData.py
+++ src/pushData.py
@@ -22,7 +22,6 @@ outlet = StreamOutlet(info)
 # for file in os.listdir('directory'):
 # for i in range(0,len(images)):
 
-
 # def display(files, .....):
 #   ex: file_name = ['/dir/dir2/img.png']
 
@@ -33,22 +32,20 @@ print(cfg.trial_order)
 #trial_order = ['one', 'two', 'one']
 
 for element in cfg.trial_order: #loop through all elements in array trial_order
-  trials['one']                 #trial is a dictionary, 'one' is a keyword
-# print(trials['one'].stimuli_type)    
-  
-imageIndex = 0
-for imageIndex in range(len(cfg.trials[element].files)):
+  imageIndex = 0
+  for imageIndex in range(len(cfg.trials[element].files)):
 
- stimulis = cfg.trials[element].files[imageIndex]
+    stimulis = cfg.trials[element].stimuli_folder + '/' + cfg.trials[element].files[imageIndex]
     showStim = visual.ImageStim(window, stimulis)
     #visual.ImageStim(window, image = stimulus)
     showStim.draw([window])
     window.flip()
     core.wait(2.0) 
     #stimuli_running = True
-    
+
     #if statement to differentiate between images and audio files
-   
+
+    '''
     #first element: type of stimuli, second: the filename, third: wait time
     mysample = [stimuli_type, stimulis, time_placeholder]
     outlet.push_sample(mysample)                
@@ -62,71 +59,7 @@ for imageIndex in range(len(cfg.trials[element].files)):
     mysample = [stimuli_type, stimulis, time_placeholder]
     print("now sending data...")
     outlet.push_sample(mysample)                
-    
+    '''
+
     time.sleep(0.01)
- 
 
-
-#while True:
-#mysample = []
-
-#-------------------#
-
-''' import open_bci_v3 as bci
-
-NUM_CHANNELS = 8
-data = []
-
-def handleSample(sample):
-    channels = sample.channel_data[0:NUM_CHANNELS]
-    data.append(channels)
-
-    print(channels)
-
-def main():
-    board = bci.OpenBCIBoard(port='/dev/ttyUSB0', filter_data=True, daisy=False)
-    print (board.getNbEEGChannels(), "EEG channels and", 
-            board.getNbAUXChannels(), "AUX channels at", 
-            board.getSampleRate(), "Hz.")
-
-    #data.append([])
-
-    board.start_streaming(handleSample)
-
-    #while True:
-    pass
-
-if __name__ == '__main__':
-    main(
-
-#----------------------#
-
-import pylsl
-from psychopy import prefs
-prefs.general['audioLib'] = ['pygame']
-from psychopy import visual, core, sound 
-
-def main():
-    win = visual.Window([512, 512])                 #defines the frame to show
-
-    # writes text to win on the back buffer
-    message = visual.TextStim(win, text='ehllo') 
-    message.setAutoDraw(True)
-    win.flip() # displays back buffer to front
-
-    core.wait(1.0)
-
-    for i in xrange(0, 10):
-      if i % 2:
-        message.setText('odd')
-      else:
-        message.setText('even')
-
-      win.flip()
-      core.wait(1.0)
-
-  sound.Sound('stimulus-400.wav').play()
-  core.wait(2.0)
-
-if __name__ == '__main__':
-    main( '''
