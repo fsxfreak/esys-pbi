6860d640b2cc1576cdfb3223bebe403a87ef4edb
kvkung <kvkung@ucsd.edu>
2017-03-25 01:49:14 -0700

    Update randomNums.py
    update randomNums.py (correctly passes time stamp and data [ie random number from 1-10] into outlet)

--- src/randomNums.py
+++ src/randomNums.py
@@ -1,18 +1,24 @@
-mport numpy as np
+import numpy as np
 import random
 from pylsl import StreamInfo, StreamOutlet
+import time
 
 
-info  = StreamInfo('bci')
+info = StreamInfo('bci', 'randomData', 2, 100, 'float32', 'myuid34234')
+#info = StreamInfo('bci')
 outlet = StreamOutlet(info)
 
-for timestamp in range(0,1000):
-    sample = random.randint(0,1000)
+print("about to go into for loop")
+
+for timestamp in range(0,10):
+    sample = random.randint(0,10)
     data = [sample, timestamp]
+    #data = [sample]
     outlet.push_sample(data)
+    print(data)
     time.sleep(0.01)
 
- print("sending data now")
+print("finished sending data")
 
 
     
